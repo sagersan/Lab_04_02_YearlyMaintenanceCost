45742d8f2cf51e8cdfcda3be6f82abd652ff428c
sagersan <sagersan@mail.uc.edu>
2026-09-18 13:05:39 -0400

    Initial commit

---
+++ src/YearlyCost2.java
@@ -0,0 +1,18 @@
+public class YearlyCost2
+    {
+        static void main ()
+        {
+            double spring = 120.50;
+            double fall = 360.52;
+            double winter = 503.46;
+            double summer = 789.10;
+
+            double total = 0;
+
+            total = spring + fall + winter + summer;
+
+            IO.println("The total cost is " + total);
+        }
+    }
+
+
